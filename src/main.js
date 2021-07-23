5c20a900862aabcc62a490fa15755a1cd01c5440
nroy6967 <ncroy7@gmail.com>
2021-07-23 11:23:32 -0400

    a few tinkers from the original hello world

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+/*const application = Vue.createApp({
+  data(){
+    return{
+      author: 'Nick',
+      sample: 'this is a sample text'
+    }
+  }
+})
+application.mount('#app')*/
 new Vue({
   render: h => h(App),
 }).$mount('#app')
